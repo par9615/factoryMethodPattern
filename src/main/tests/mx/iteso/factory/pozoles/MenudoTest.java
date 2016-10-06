5108e5765d87482a637e8d10f1fac12682e9a726
Kireck211 <erickdesantiago11@gmail.com>
2016-10-05 19:49:52 -0500

    Creation of some tests

---
+++ src/main/tests/mx/iteso/factory/pozoles/MenudoTest.java
@@ -0,0 +1,7 @@
+package mx.iteso.factory.pozoles;
+
+/**
+ * Created by Erick on 05/10/2016.
+ */
+public class MenudoTest {
+}
---
+++ src/main/tests/mx/iteso/factory/pozoles/PozolilloTrompaTest.java
@@ -0,0 +1,10 @@
+package mx.iteso.factory.pozoles;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Erick on 05/10/2016.
+ */
+public class PozolilloTrompaTest {
+
+}
